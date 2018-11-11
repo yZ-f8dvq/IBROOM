03d2a7c0f6c84300be3f9c621fe1e7ce02ab2956
yZ-f8dvq <44929429+yZ-f8dvq@users.noreply.github.com>
2018-11-11 21:27:34 +0100

    Update index.php

--- IBroom/index.php
+++ IBroom/index.php
@@ -5,19 +5,6 @@
 
 		<main>
 			
-	<!--<form action="includes/signup.inc.php" method="POST">
-		<input type="text" name="first" placeholder="First name">
-		<br>
-		<input type="text" name="last" placeholder="Last name">
-		<br>
-		<input type="text" name="email" placeholder="example@email.com">
-		<br> 
-		<input type="text" name="uid" placeholder="Username">
-		<br>
-		<input type="password" name="pwd" placeholder="Password">
-		<br>
-		<button type="submit" name="submit"> Sign Up</button>
-	</form>-->
 			<h1>Welcome to the IB room</h1>
 			<p>This is the IB Forum for us students. <br />Here we can post about anything relating to school or the IB diploma.</p>
 		
