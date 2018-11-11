19ab17b00c5604a12fc05f1c2307c1b3e76eb48f
yZ-f8dvq <44929429+yZ-f8dvq@users.noreply.github.com>
2018-11-11 21:26:55 +0100

    Update register.php

--- IBroom/register.php
+++ IBroom/register.php
@@ -3,7 +3,8 @@
 	?>
 <main class="registerContainer">
 	<h2>register</h2>
-	
+		
+		
 		<form class="register" action="includes/signup.inc.php" method="POST">
 		<input type="text" name="uid" placeholder="Username">
 		<br> 
@@ -13,8 +14,31 @@
 		<br>
 		<input type="password" name="pwd-repeat" placeholder="Repeat password">
 		<br>
-		<button type="submit" name="signup-submit"> Sign Up</button>
+		<?php 
+		 if (isset($_GET['error'])) {
+		 	if ($_GET['error'] == "emptyfields") {
+		 		echo '<p>Fill in all fields!</p>';
+		 	}
+		 	else if ($_GET['error'] == "invalidmailuid") {
+		 		echo '<p>Invalid username and e-mail!</p>';
+		 	}
+		 	else if ($_GET['error'] == "invalidmail") {
+		 		echo '<p>Invalid e-mail!</p>';
+		 	}
+		 	else if ($_GET['error'] == "passwordcheck") {
+		 		echo '<p>Your passwords do not match!</p>';
+		 	}
+		 	else if ($_GET['error'] == "usertaken") {
+		 		echo '<p>Username already taken!</p>';
+		 	}
+		 }
+		 /*else if (($_GET['signup'] == "success") {
+
+		 }*/
+		 ?>
+		<button type="submit" name="signup-submit"> Sign Up!</button>
 	</form>
+
 </main>
 	<?php
 	require "footer.php";
