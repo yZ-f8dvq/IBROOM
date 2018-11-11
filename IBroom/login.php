f144af3952a73916deb7818924e0b117e8d3ccae
yZ-f8dvq <44929429+yZ-f8dvq@users.noreply.github.com>
2018-11-11 21:26:20 +0100

    Update login.php

--- IBroom/login.php
+++ IBroom/login.php
@@ -2,32 +2,20 @@
 	require "header.php";
 	?>
 
-	<main>
+	<main class="registerContainer">
 		<h2>Log in</h2>
-		<?php
-			if (isset($_SESSION['userId'])) {
-				echo '<form action="includes/logout.inc.php" method="post">
-					<button type="submit" name="logout-submit">Logout</button>
-					</form>';
-			}
-			else {
-				echo "<p>You are logged out</p>";
-			}
-		?>
-		<form action="includes/login.inc.php" method="post">
+		
+		<form class="register" action="includes/login.inc.php" method="post">
 			<input type="text" name="mailuid" placeholder="Username/E-mail">
+			<br>
 			<input type="text" name="pwd" placeholder="Password">
-			<button type="submit" name="login-submit">Login</button>
+			<br>
+			<button type="submit" name="login-submit">Log in!</button>
 		</form>
 		<a href="register.php">Not registered yet?</a>
 		
 
 	</main>
-
-
-
-
-
 
 
 
